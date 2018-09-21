44c2c7677ad6061b6c92ae48c8a33cec4ada22db
Rebecca Cedermalm <rebce973@student.liu.se>
2018-09-21 16:54:23 +0200

    Changed large background image and added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Projects from './components/Projects.js';
 import GeneralInfo from './components/GeneralInfo.js';
 import './css/App.css';
 import profile_image from './images/profile_image.png';
+import { Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -15,6 +16,20 @@ class App extends Component {
         </header>
         <GeneralInfo/>
         <Projects/>
+        <div className="footer">   
+          <Container className="footer-body">
+            <Row>
+              <Col className="footer-column">
+                <p>Great work! You have made it to the end of my homepage. Have some cake to celebrate!</p>
+                <p>Hopefully you learned something new about me and I am thanking you for taking your time looking though all of this. Feel free to contact me if you have any questions.</p>
+              </Col>
+              <Col className="footer-column">
+              <i class="fas fa-birthday-cake cake-icon align-middle"></i> 
+              </Col>
+            </Row>
+          </Container>
+          <p className="copyright">© Rebecca Cedermalm</p>  
+        </div>
       </div>
     );
   }
